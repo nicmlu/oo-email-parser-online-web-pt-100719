dc46fbba134f15b7502b2bffcf1e3ccd5e8f084b
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-04 04:53:04 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,8 +7,8 @@ class EmailAddressParser
   attr_accessor :email_addresses
 
   def parse
-    email_address_parser = email_address_parser.new
-    email_arr = email_address_parser.split(/, /)
+    email_address_parser = email_address_parser.new(email_addresses)
+    # email_arr = email_address_parser.split(/, /)
   end
 
 end
